a31aabad610263ba7d4c8416ea7707e684fcc603
Amahle-29 <zungusithabileamahle@gmail.com>
2026-08-26 19:49:51 +0200

    Refactor HomeController to use readonly context
    Changed the messages to make system more user friendly and professional giving meaningful feedback to users.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,23 +9,23 @@ namespace HarborConnect.Controllers
 {
     public class HomeController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly ApplicationDbContext context = new ApplicationDbContext();
         public ActionResult Index()
         {
-            var categories = db.BoatCategories.ToList();
+            var categories = context.BlogCategories.ToList();
             return View(categories);
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Welcome to HarborConnect - learn more about us here.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Reach out to us anytime.";
 
             return View();
         }
